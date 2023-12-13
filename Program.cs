865776dbf5fda48c3d81578b8847dddfd15fa441
Luiz Henrique Rosa Da Silva <luizsilva132132@outlook.com>
2023-12-12 21:35:03 -0300

    Desafio trilha-net-poo celulares concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "+5514998765432", modelo: "Nokia Star", imei: "987654321", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+System.Console.WriteLine();
+
+System.Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "+5514991234567", modelo: "Iphone 14", imei: "123456789", memoria: 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
